9e033af8f638b52cbdeeed09e9cd4a25e1c7455a
manjurulhoque <rumimanzurulhoque@gmail.com>
2017-12-06 19:17:19 +0600

    gig model and controller added

---
+++ database/migrations/2017_12_06_131641_create_gigs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gigs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gigs');
+    }
+}
