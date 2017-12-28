8a39b6b1a14e141545f1b331c914d2d90b5bfc5d
manjurulhoque <rumimanzurulhoque@gmail.com>
2017-12-28 17:45:37 +0600

    review model, controller, migration

---
+++ database/migrations/2017_12_28_044815_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+	        $table->integer('user_id')->unsigned();
+	        $table->integer('gig_id')->unsigned();
+	        $table->foreign('gig_id')->references('id')->on('gigs')->onDelete('cascade');
+	        $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
