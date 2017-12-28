1ba7784db237eedc0ad83fd407432499ac6c8071
manjurulhoque <rumimanzurulhoque@gmail.com>
2017-12-28 17:57:47 +0600

    purchase model, controller, migration

---
+++ database/migrations/2017_12_28_115535_create_purchases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->increments('id');
+	        $table->integer('user_id')->unsigned();
+	        $table->integer('gig_id')->unsigned();
+	        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
